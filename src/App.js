e21a15d222e38649ba6b644b0db1d7f1508b80f1
kshitij-404 <kshitij4ka@gmail.com>
2021-11-15 06:34:03 +0530

    after trying 2nd route

--- src/App.js
+++ src/App.js
@@ -3,73 +3,69 @@ import "./App.css";
 //import SearchBar from "./Components/SeachBar";
 //import JsonData from "./Data.json";
 import React, { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import reactDom from "react-dom";
-import { render } from "react-dom";
+// import { render } from "react-dom";
 import SearchBar from "./Components/SeachBar";
 import CallDuration from "./Components/CallDuration";
 import AgentTable from "./Components/AgentTable";
 import Fiftycall from "./Components/fiftycall";
+import Agent from "./Components/agent";
 
 function App() {
-  const [data, setData] = useState([]);
-  const [selAgents, setSelAgents] = useState([]);
-  const [entermin, setEntermin] = useState("0");
-  const [entermax, setEntermax] = useState("0");
-  const [filAgents, setFilAgents] = useState([]);
+  // const [data, setData] = useState([]);
+  // const [selAgents, setSelAgents] = useState([]);
+  // const [entermin, setEntermin] = useState("0");
+  // const [entermax, setEntermax] = useState("0");
+  // const [filAgents, setFilAgents] = useState([]);
+
+  // // useEffect(() => {
+  // //   data();
+  // //   }, []);
 
   // useEffect(() => {
-  //   data();
-  //   }, []);
+  //   fetch("https://damp-garden-93707.herokuapp.com/getlistofagents")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       // console.log(data.data.listofagents);
+  //       setData(data.data.listofagents);
+  //     });
+  // }, []);
 
-  useEffect(() => {
-    fetch("https://damp-garden-93707.herokuapp.com/getlistofagents")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        // console.log(data.data.listofagents);
-        setData(data.data.listofagents);
-      });
-  }, []);
+  // useEffect(() => {
+  //   console.log(selAgents);
 
-  useEffect(() => {
-    console.log(selAgents);
-
-    fetch("https://damp-garden-93707.herokuapp.com/getfilteredcalls", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        info: {
-          filter_agent_list: selAgents,
-          filter_time_range: [parseFloat(entermin), parseFloat(entermax)],
-        },
-      }),
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log("hi", data);
-        setFilAgents(data.data);
-      });
-  }, [selAgents, entermin, entermax]);
+  //   fetch("https://damp-garden-93707.herokuapp.com/getfilteredcalls", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       info: {
+  //         filter_agent_list: selAgents,
+  //         filter_time_range: [parseFloat(entermin), parseFloat(entermax)],
+  //       },
+  //     }),
+  //   })
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       console.log("hi", data);
+  //       setFilAgents(data.data);
+  //     });
+  // }, [selAgents, entermin, entermax]);
 
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route exact path="/fifty" element={<Fiftycall />} />
-          <Route exact path="/" element={<App />} />
-        </Routes>
+        <Switch>
+          <Route exact path="/agent" component={Agent} />
+          <Route exact path="/fifty" component={Fiftycall} />
+          <Route exact path="/" component={App} />
+        </Switch>
 
         {/* <SearchBar
           selectedAgents={data}
--- src/Components/fiftycall.js
+++ src/Components/fiftycall.js
@@ -2,9 +2,15 @@ import React from "react";
 import { useState, useEffect } from "react";
 import AddLabel from "./addLabel";
 import CallerTable from "./callerTable";
+import CallerDrop from "./calldropdown";
 
 function Fiftycall() {
   const [filter, setFilter] = useState([]);
+  const [min, setMin] = useState([]);
+  const [max, setMax] = useState([]);
+  const [callers, setCallers] = useState([]);
+  const [callid, setCallid] = useState([]);
+  const [finFilter, setFinFilter] = useState([]);
   const [selectedFilter, setSelectedFilter] = useState([]);
 
   useEffect(() => {
@@ -24,11 +30,36 @@ function Fiftycall() {
       });
   }, []);
 
+  useEffect(() => {
+    fetch("https://damp-garden-93707.herokuapp.com/getcalllist", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        user_id: "24b456",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.data.call_data[0].label_id);
+        console.log(
+          data.data.call_data[0].call_id,
+          data.data.call_data[data.data.call_data.length - 1].call_id
+        );
+        setMin(data.data.call_data[0].call_id);
+        setMax(data.data.call_data[data.data.call_data.length - 1].call_id);
+        setCallers(data.data.call_data);
+        // setFinFilter(data.data.call_data[callid].label_id);
+      });
+  }, []);
+
   return (
     <div>
+      <CallerDrop min={min} max={max} callid={callid} setCallid={setCallid} />
       <AddLabel filter={filter} setSelectedFilter={setSelectedFilter} />
       <br />
-      <CallerTable />
+      {/* <CallerTable callers={finFilter} /> */}
     </div>
   );
 }
